0ce9305c0880e83a8538526c537c9fe77858003a
Hammam <hajeddawi@gmail.com>
2024-02-13 14:20:30 +0300

    fix it

--- weather_data_agg.py
+++ weather_data_agg.py
@@ -1,41 +1,51 @@
 weather_data:dict = {}
 
-def input_data ():
+def input_data (city):
     '''this function for getting inputs from user and store it in nested dictionary'''
-    city:str = input('Please enter the city name: ')
     date:str = input('Please enter a date ( DD - MM - YYYY): ')
     temperature:int = int(input('Please enter the temperature (°C): '))
     humidity:str = int(input('Please enter the humidity (%): '))
     weather_condition:str = input('Please enter the weather condition (e.g., sunny, rainy): ')
 
-    weather_data[city] = {
-        'date':date,
+    if city not in weather_data:
+        weather_data[city] = {}
+
+    weather_data[city] [date]= {
         'temperature':temperature,
         'humidity':humidity,
         'weather_condition':weather_condition
     }
+    input('')
 
-def query_by_city():
-    city_name = input('Please enter city name: ')
-    if city_name in weather_data:
-        for key, value in weather_data[city_name].items():
-            print(f"{key}: {value}")
+def query_weather_data(city:str):
+    
+    if city in weather_data:
+        city_data = weather_data[city]
+        for date in city_data:
+            print(f"weather for {city} on {date}: ")
+            print(f"Temperature: {city_data[date]['temperature']} °C")
+            print(f"Humdity: {city_data[date]['humidity']} %")
+            print(f"Condition: {city_data[date]['weather_condition']}")
+            input("")
+
     else:
-        print("City not found in the data.")
-        
+        print("No weather data for this city!")
 
-
-while True:
-    print("\n1. Input Weather Data\n2. Query by City\n3. Exit")
-    choice = int(input("Enter your choice: "))
-    if choice == 1:
-        input_data()
-    elif choice == 2:
-        query_by_city()
-    elif choice == 3:
-        print('Exiting from the program...')
-        break
-    else:
-        print('Enter a valid number: ')
-
+try:
+    while True:
+        print("\n1. Input Weather Data\n2. Query by City\n3. Exit")
+        choice = int(input("Enter your choice: "))
+        if choice == 1:
+            city = input('Please Enter The City Name: ')
+            input_data(city)
+        elif choice == 2:
+            city = input('Please Enter The City Name: ')
+            query_weather_data(city)
+        elif choice == 3:
+            print('Exiting from the program...')
+            break
+        else:
+            print('Enter a valid number: ')
+except ValueError:
+    print('You enter a wrong inputs. Try again')
         
